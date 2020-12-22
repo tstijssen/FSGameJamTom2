466aa32101bb94c3c6525f41e7d48715a0263d6d
Tom Kendall <tomkendall89@gmail.com>
2020-12-22 11:17:35 +0000

    Added menu

---
+++ Assets/Scripts/TextEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextEffect : MonoBehaviour
+{
+    Color lerpedColor = Color.red;
+    Text title;
+    // Start is called before the first frame update
+    void Start()
+    {
+        title = GetComponent<Text>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lerpedColor = Color.Lerp(Color.red, Color.yellow, Mathf.PingPong(Time.time, 1));
+        title.color = lerpedColor;
+    }
+}
