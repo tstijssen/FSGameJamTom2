01a37ae3e72a56eb2285792c73161c7e8ac4d199
Tom Kendall <tomkendall89@gmail.com>
2020-12-22 14:17:57 +0000

    Readded Stuff

--- Assets/Scripts/CustomNetworkRoomPlayer.cs
+++ Assets/Scripts/CustomNetworkRoomPlayer.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using Mirror;
 
 public class CustomNetworkRoomPlayer : NetworkRoomPlayer
@@ -11,11 +10,6 @@ public class CustomNetworkRoomPlayer : NetworkRoomPlayer
     public string playerName;
     [SyncVar]
     public int playerType;
-    
-    public GameObject playerUI;
-    public Text playerNameText;
-    public Text playerReadyText;
-    public Image playerSprite;
 
     public Sprite[] typeSprites;
 
@@ -27,62 +21,5 @@ public class CustomNetworkRoomPlayer : NetworkRoomPlayer
 
         PlayerPrefs.SetString("playerName", playerName);
         PlayerPrefs.SetInt("playerType", playerType);
-    }
-
-    public override void OnGUI()
-    {
-        NetworkRoomManager room = NetworkManager.singleton as NetworkRoomManager;
-        if (room || !NetworkManager.IsSceneActive(room.RoomScene))
-        {
-            if(!playerUI.activeInHierarchy)
-            {
-                GameObject lobby = GameObject.FindWithTag("Lobby");
-
-                if(lobby)
-                {
-                    if(lobby.TryGetComponent(out GridLayoutGroup grid))
-                    {
-                        this.transform.SetParent(lobby.transform);
-                    }
-                }
-                playerType = index;
-                playerName = "Player " + (index + 1);
-
-                PlayerPrefs.SetString("playerName", playerName);
-                PlayerPrefs.SetInt("playerType", playerType);
-
-                playerNameText.text = playerName;
-                playerUI.SetActive(true);
-                playerSprite.sprite = typeSprites[index];
-            }
-        }
-        else if (playerUI.activeInHierarchy)
-        {
-            playerUI.SetActive(false);
-            this.transform.SetParent(null);
-        }
-    }
-
-    public void SetPlayerReady()
-    {
-        if (NetworkClient.active && isLocalPlayer && !readyToBegin)
-        {
-            CmdChangeReadyState(true);
-            playerReadyText.text = "Ready!";
-        }
-    }
-
-    public void UnSetPlayerReady()
-    {
-        if (NetworkClient.active && isLocalPlayer && readyToBegin)
-        {
-            CmdChangeReadyState(false);
-            playerReadyText.text = "Not ready!";
-        }
-    }
-
-    public void RemovePlayer()
-    {
-
     }
 }
